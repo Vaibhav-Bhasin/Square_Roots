99e13f21991e2f1417eb9a1e77fd8930736ab902
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-14 22:35:51 +0530

    Update SQ_^Roots^.py

--- SQ_^Roots^.py
+++ SQ_^Roots^.py
@@ -1,2 +1,8 @@
 import random
 w = random.randint(1,5)
+for j in range(w):
+  print(w)
+  i = int(input("Please Enter Any Number Between 1 to 30 To Find Out Its Squaroot"))
+  #formula = i^1/2
+  print(i)
+  g=(i**1/2)
